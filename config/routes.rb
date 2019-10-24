4ae46b75ba20d70ae5427abce077753f3f832890
Patrick Horne <patrickahorne@gmail.com>
2019-10-24 19:33:49 +1100

    Add scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get root :to => 'pages#home'
+  get '/about' => 'pages#about'
+  get '/login' => 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
