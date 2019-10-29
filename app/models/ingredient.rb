c901d2d99f42af8a808abaed7fd2b9e8945ae1af
Patrick Horne <patrickahorne@gmail.com>
2019-10-29 14:21:19 +1100

    Start seedfile, update ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,3 @@
 class Ingredient < ApplicationRecord
-  has_many :recipes through :ingredients_recipes
+  has_many :recipes, through: :ingredients_recipes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-seed_recipe_data =[
+seed_recipe_data = [
   {
-    recipe_name => 'Scrambled Eggs',
-    recipe_ingredients => ['Eggs', 'Milk', 'Butter'],
-    recipe_instructions => ['Crack eggs', 'Whisk eggs', 'Add milk', 'Fry eggs', 'Serve']
+    :recipe_name => 'Scrambled Eggs',
+    :recipe_ingredients => ['Eggs', 'Milk', 'Butter'],
+    :recipe_instructions => ['Crack eggs', 'Whisk eggs', 'Add milk', 'Fry eggs', 'Serve']
   }, {
-    recipe_name => 'Cereal',
-    recipe_ingredients => ['Cereal', 'Milk'],
-    recipe_instructions => ['Pour Cereal into bowl', 'Add Milk', 'Enjoy!']
+    :recipe_name => 'Cereal',
+    :recipe_ingredients => ['Cereal', 'Milk'],
+    :recipe_instructions => ['Pour Cereal into bowl', 'Add Milk', 'Enjoy!']
   }, {
-    recipe_name => 'PB&J Sandwich',
-    recipe_ingredients => ['Bread', 'Peanut Butter', 'Jam'],
-    recipe_instructions => ['Spread peanut butter on one slice of bread', 'Spread jam on the other slice of bread', 'Make a sandwich', 'Enjoy!']
+    :recipe_name => 'PB&J Sandwich',
+    :recipe_ingredients => ['Bread', 'Peanut Butter', 'Jam'],
+    :recipe_instructions => ['Spread peanut butter on one slice of bread', 'Spread jam on the other slice of bread', 'Make a sandwich', 'Enjoy!']
   }
 ]
 
+seed_recipe_data.each do |entry|
+  this_recipe = Recipe.create!(
+    :title => entry.recipe_name
+  )
+  entry.recipe_instructions.each do |item|
+    Instruction.create!(
+      :content => item,
+      :recipe_id => this_recipe.id
+    )
+  end
+  
+  entry.recipe_ingredients.each do |item|
+    Ingredient.create!(
+      :name => item
+    )
+  end
+end
 
-
-users = User.create([
+users = User.create!([
   {
     username: 'glhc',
     first_name: 'Patrick',
